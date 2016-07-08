921be374f437a6e13c53608319f59bec531107be
KarenCaballeros <anakarencaballeros@gmail.com>
2016-07-07 20:44:57 -0600

    sigo intentando.

--- funciones.py
+++ funciones.py
@@ -67,27 +67,70 @@ def cjug2(elec,lista):
 			return "Error"					
 	
 def gano(lista):
-	lista[0][0] = 1
-	lista[0][0] = 2
-	lista[0][0] = 3
-	lista[0][0] = 4
-	lista[0][0] = 5
-	lista[0][0] = 6
-	lista[0][0] = 7
-	lista[0][0] = 8
-	lista[0][0] = 9
 	
 	if lista[0][0] == lista[0][1] and lista[0][0] == lista[0][2]:
+		gano = True
+		if lista[0][0] == "X":
+			return "JUGADOR X GANO"
+		elif lista[0][0] == "O":
+			return "JUGADOR O GANO"	
 
-	if lista[1][0] == lista[1][1] and lista[1][0] == lista[1][2]:
+	elif lista[1][0] == lista[1][1] and lista[1][0] == lista[1][2]:
+		gano = True
+		if lista[1][0] == "X":
+			return "JUGADOR X GANO"
+		elif lista[1][0] == "O":
+			return "JUGADOR O GANO"
 	
-	if lista[2][0] == lista[2][1] and lista[2][0] == lista[2][2]:
+	elif lista[2][0] == lista[2][1] and lista[2][0] == lista[2][2]:
+		gano = True
+		if lista[2][0] == "X":
+			return "JUGADOR X GANO"
+		elif lista[2][0] == "O":
+			return "JUGADOR O GANO"
 
-	if lista[0][0] == lista[1][0] and lista[0][0] == lista[2][0]:
+	elif lista[0][0] == lista[1][0] and lista[0][0] == lista[2][0]:
+		gano = True
+		if lista[0][0] == "X":
+			return "JUGADOR X GANO"
+		elif lista[0][0] == "O":
+			return "JUGADOR O GANO"
 
-	if lista[0][1] == lista[1][1] and lista[0][0] == lista[2][1]:
+	elif lista[0][1] == lista[1][1] and lista[0][1] == lista[2][1]:
+		gano = True
+		if lista[0][1] == "X":
+			return "JUGADOR X GANO"
+		elif lista[0][1] == "O":
+			return "JUGADOR O GANO"
 
-	if lista[0][2] == lista[][1] and lista[0][0] == lista[0][2]:
+	elif lista[0][2] == lista[1][2] and lista[0][2] == lista[2][2]:
+		gano = True
+		if lista[0][2] == "X":
+			return "JUGADOR X GANO"
+		elif lista[0][2] == "O":
+			return "JUGADOR O GANO"
 
-	if lista[0][0] == lista[0][1] and lista[0][0] == lista[0][2]:	
+	elif lista[0][0] == lista[1][1] and lista[0][0] == lista[2][2]:
+		gano = True
+		if lista[0][0] == "X":
+			return "JUGADOR X GANO"
+		elif lista[0][0] == "O":
+			return "JUGADOR O GANO"
 
+	elif lista[0][2] == lista[1][1] and lista[0][2] == lista[2][0]:	
+		gano = True
+		if lista[0][2] == "X":
+			return "JUGADOR X GANO"
+		elif lista[0][2] == "O":
+			return "JUGADOR O GANO"	
+		else:
+			gano= False	
+
+def repetir(seguir):
+	print("Jugador X ")
+	elec= input("Ingrese la letra de la ubicacion deseada: ")
+	print(funciones.cjug1(elec,lista))
+	print("Jugador O ")
+	elec= input("Ingrese la letra de la ubicacion deseada: ")
+	print(funciones.cjug2(elec,lista))
+	return " "
--- totito.py
+++ totito.py
@@ -5,9 +5,28 @@ lista= [["a","b","c"],
 		]
 print("Bienvenido a totito.")
 print(funciones.mostrar(lista))	
+
 print("Jugador X ")
 elec= input("Ingrese la letra de la ubicacion deseada: ")
 print(funciones.cjug1(elec,lista))
 print("Jugador O ")
 elec= input("Ingrese la letra de la ubicacion deseada: ")
 print(funciones.cjug2(elec,lista))
+print(funciones.gano(lista))
+
+cont = 4
+while cont >= 0:
+	print("Jugador X ")
+	elec= input("Ingrese la letra de la ubicacion deseada: ")
+	print(funciones.cjug1(elec,lista))
+	print(funciones.gano(lista))
+	print("Jugador O ")
+	elec= input("Ingrese la letra de la ubicacion deseada: ")
+	print(funciones.cjug2(elec,lista))
+	print(funciones.gano(lista))
+	cont= cont -1
+
+
+
+
+	
